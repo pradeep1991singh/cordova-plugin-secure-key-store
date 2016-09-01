d73d6f822978bd695209cfdf0871a3308c70937a
pradeep <pradeep1991singh@gmail.com>
2016-09-01 20:49:49 +0530

    plugin scafolding

---
+++ src/android/SecureKeyStore.java
@@ -0,0 +1,36 @@
+package cordova
+
+import java.lang.String;
+
+-plugin-secure-key-store;
+
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * This class echoes a string called from JavaScript.
+ */
+public class SecureKeyStore extends CordovaPlugin {
+
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        if (action.equals("coolMethod")) {
+            String message = args.getInt(0);
+            this.coolMethod(minutes, callbackContext);
+            return true;
+        }
+        return false;
+    }
+
+    private void coolMethod(String message, CallbackContext callbackContext) {
+        if (message != null && message.length() > 0) {
+            callbackContext.success(message);
+        } else {
+            callbackContext.error("Expected one non-empty string argument.");
+        }
+    }
+}
