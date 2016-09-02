e641270372fc26a8eaefa17ca5b8113d70d0cb88
pradeep <pradeep1991singh@gmail.com>
2016-09-02 11:50:05 +0530

    added log

--- src/android/SecureKeyStore.java
+++ src/android/SecureKeyStore.java
@@ -1,4 +1,5 @@
 package com.securekeystore.plugins;
+import com.securekeystore.log;
 
 import java.lang.String;
 
@@ -18,8 +19,6 @@ import android.os.Bundle;
 import android.security.KeyPairGeneratorSpec;
 import android.support.v4.app.Fragment;
 import android.util.Base64;
-
-import com.securekeystore.log;
 
 import java.io.IOException;
 import java.math.BigInteger;
